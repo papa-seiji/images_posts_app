c1f4c241dcf4ea74092751843cd076e8b45a9f74
papa-seiji <iwillbestandbyme@gmail.com>
2020-03-17 02:17:23 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ImagesPostsApp
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
