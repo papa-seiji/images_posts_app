5692a8bc1c8b58690d6f955f2df712dd8645da5e
papa-seiji <iwillbestandbyme@gmail.com>
2020-03-16 21:46:17 +0900

    デプロイのための設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ImagesPostsApp
   class Application < Rails::Application
+    config.assets.initialize_on_precompilke = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
