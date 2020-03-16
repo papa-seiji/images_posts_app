d309d6c69a8069ab6c7cf2d1d5f715884474f654
papa-seiji <iwillbestandbyme@gmail.com>
2020-03-16 22:51:08 +0900

    デプロイのための設定1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,13 +24,12 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.serve_static_assets = true
+  config.assets.compile = false
 
-  `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
